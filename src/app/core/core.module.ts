cded44518b62e1dd774497dbbe94b2abbed9ae73
guilhermescr <guilhermechaves2005@hotmail.com>
2023-07-04 23:27:47 -0300

    feat: add about-me section

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,21 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, IntroductionComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    IntroductionComponent,
+    AboutMeComponent,
+  ],
   imports: [CommonModule, SharedModule],
-  exports: [HeaderComponent, FooterComponent, IntroductionComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    IntroductionComponent,
+    AboutMeComponent,
+  ],
 })
 export class CoreModule {}
